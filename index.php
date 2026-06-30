89035082527e0a306bd860f44584392e54670d75
root <root@um9306.localdomain>
2026-06-30 20:00:44 +0900

    메인 화면 추가

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>K.Knock 게시판</title>
+</head>
+<body>
+    <h1>K.Knock 게시판</h1>
+    <p>원하는 게시판을 선택해주세요.</p>
+    <ul>
+        <li><a href="board/index.php">자유게시판</a></li>
+        <li><a href="notice/index.php">공지사항</a></li>
+    </ul>
+</body>
+</html>
